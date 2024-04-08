432d020931cd4260e0be8d46501fec215e25ff2a
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-04-08 18:24:22 +0900

    Title: 코드트리 오마카세, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240408/코드트리 오마카세/codetree-omakase.py
+++ 240408/코드트리 오마카세/codetree-omakase.py
@@ -2,58 +2,70 @@ L, Q = map(int, input().split())
 
 order = [list(input().split()) for _ in range(Q)]
 
-s_lst = [[] for _ in range(L)]
-p_lst = [[] for _ in range(L)]
-seat_lst = []
+s_lst = dict()
+p_lst = dict()
 
 t = 0
 o_cnt = 0
 
 
-def action(o):
-    if o[0] == "100":
+def action(ord):
+    if ord[0] == "100":
         # 초밥 제작
-        s_lst[int(o[2])].append(o[3])
-    elif o[0] == "200":
+        if int(ord[2]) in s_lst:
+            temp = s_lst.get(int(ord[2]))
+        else:
+            temp = []
+        temp.append(ord[3])
+        s_lst[int(ord[2])] = temp
+    elif ord[0] == "200":
         # 손님 착석
-        p_lst[int(o[2])] = [o[3], int(o[4])]
-        seat_lst.append(int(o[2]))
-    elif o[0] == "300":
+        p_lst[int(ord[2])] = [ord[3], int(ord[4])]
+    elif ord[0] == "300":
         # 사진 찍기
-        p_cnt = len(seat_lst)
+        p_cnt = len(p_lst)
         s_cnt = 0
         for s in s_lst:
-            s_cnt += len(s)
+            s_cnt += len(s_lst.get(s))
         print(p_cnt, s_cnt)
 
 
 def chk_sushi():
-    for idx in seat_lst:
+    p_key = list(p_lst.keys())
+    for idx in p_key:
         while True:
-            if p_lst[idx][0] in s_lst[idx]:
-                s_lst[idx].remove(p_lst[idx][0])
-                p_lst[idx][1] -= 1
-                if p_lst[idx][1] == 0:
-                    p_lst[idx] = []
-                    seat_lst.remove(idx)
+            if idx in s_lst:
+                if p_lst[idx][0] in s_lst[idx]:
+                    tmp = s_lst[idx]
+                    tmp.remove(p_lst[idx][0])
+                    s_lst[idx] = tmp
+                    p_lst[idx] = [p_lst[idx][0], p_lst[idx][1] - 1]
+                    if p_lst[idx][1] == 0:
+                        p_lst.pop(idx)
+                        break
+                else:
                     break
             else:
                 break
 
 
-while True:
-    t += 1
-    if int(order[o_cnt][1]) == t and order[o_cnt][0] != "300":
-        action(order[o_cnt])
-        o_cnt += 1
+cur_t = 1
+for o in order:
+    spent_t = int(o[1]) - cur_t
+    cur_t = int(o[1])
+    if spent_t != 0:
+        new_dict = dict()
+        for i in s_lst:
+            key = i
+            val = s_lst.get(i)
+            new_dict[(key + spent_t) % L] = val
+        s_lst = new_dict
 
-    if len(seat_lst) != 0:
+    if o[0] != "300":
+        action(o)
+
+    if len(p_lst) != 0:
         chk_sushi()
-    s_lst = [s_lst[-1]] + s_lst[:-1]
 
-    if int(order[o_cnt][1]) == t and order[o_cnt][0] == "300":
-        action(order[o_cnt])
-        o_cnt += 1
-
-    if o_cnt == Q:
-        break
+    if o[0] == "300":
+        action(o)
