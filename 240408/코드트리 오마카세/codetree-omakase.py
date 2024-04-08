42a8534d819115d2f3b536daf54650b8af9b0cc8
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-04-08 21:26:41 +0900

    Title: 코드트리 오마카세, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240408/코드트리 오마카세/codetree-omakase.py
+++ 240408/코드트리 오마카세/codetree-omakase.py
@@ -9,19 +9,19 @@ t = 0
 o_cnt = 0
 
 
-def action(ord):
-    if ord[0] == "100":
+def action(ordr):
+    if ordr[0] == "100":
         # 초밥 제작
-        if int(ord[2]) in s_lst:
-            temp = s_lst.get(int(ord[2]))
+        if int(ordr[2]) in s_lst:
+            temp = s_lst.get(int(ordr[2]))
         else:
             temp = []
-        temp.append(ord[3])
-        s_lst[int(ord[2])] = temp
-    elif ord[0] == "200":
+        temp.append(ordr[3])
+        s_lst[int(ordr[2])] = temp
+    elif ordr[0] == "200":
         # 손님 착석
-        p_lst[int(ord[2])] = [ord[3], int(ord[4])]
-    elif ord[0] == "300":
+        p_lst[int(ordr[2])] = [ordr[3], int(ordr[4])]
+    elif ordr[0] == "300":
         # 사진 찍기
         p_cnt = len(p_lst)
         s_cnt = 0
@@ -30,22 +30,29 @@ def action(ord):
         print(p_cnt, s_cnt)
 
 
-def chk_sushi():
+def chk_sushi(m):
     p_key = list(p_lst.keys())
     for idx in p_key:
-        while True:
-            if idx in s_lst:
-                if p_lst[idx][0] in s_lst[idx]:
-                    tmp = s_lst[idx]
-                    tmp.remove(p_lst[idx][0])
-                    s_lst[idx] = tmp
-                    p_lst[idx] = [p_lst[idx][0], p_lst[idx][1] - 1]
-                    if p_lst[idx][1] == 0:
-                        p_lst.pop(idx)
+        for move in range(m+1):
+            if ((idx-move) % L) in s_lst and p_lst[idx][0] in s_lst[(idx-move) % L]:
+                need_cnt = p_lst[idx][1]
+                while need_cnt:
+                    if p_lst[idx][0] in s_lst[(idx-move) % L]:
+                        tmp = s_lst[(idx-move) % L]
+                        tmp.remove(p_lst[idx][0])
+                        need_cnt -= 1
+                        if len(tmp) == 0:
+                            s_lst.pop((idx-move) % L)
+                            break
+                        else:
+                            s_lst[(idx-move) % L] = tmp
+                    else:
                         break
+                if need_cnt == 0:
+                    p_lst.pop(idx)
                 else:
-                    break
-            else:
+                    p_lst[idx] = [p_lst[idx][0], need_cnt]
+            if len(p_lst) == 0 or len(s_lst) == 0:
                 break
 
 
@@ -53,31 +60,23 @@ def chk_sushi():
 cur_t = 1
 for o in order:
     spent_t = int(o[1]) - cur_t
-    st_tmp = L if spent_t >= L else spent_t
-    for st in range(st_tmp):
-        new_dict = dict()
-        for i in s_lst:
-            key = i
-            val = s_lst.get(i)
-            new_dict[(key + 1) % L] = val
-        s_lst = new_dict
-        if len(p_lst) != 0:
-            chk_sushi()
+    if spent_t >= 2:
+        st_tmp = spent_t if spent_t < L else L
+        chk_sushi(st_tmp)
     cur_t = int(o[1])
-    st_tmp = spent_t if st_tmp == L else 0
     if spent_t != 0:
         new_dict = dict()
         for i in s_lst:
             key = i
             val = s_lst.get(i)
-            new_dict[(key + st_tmp) % L] = val
+            new_dict[(key + spent_t) % L] = val
         s_lst = new_dict
 
     if o[0] != "300":
         action(o)
 
     if len(p_lst) != 0:
-        chk_sushi()
+        chk_sushi(0)
 
     if o[0] == "300":
         action(o)
