597c679ef9b3ebd39afcd0fc0b5bffb523f16928
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-04-11 00:18:18 +0900

    Title: 코드트리 채점기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240411/코드트리 채점기/codetree-judger.py
@@ -0,0 +1,77 @@
+import heapq
+
+# w_heap = waiting heapq
+# j_queue = judging queue
+# hist_dict = history dictionary
+
+Q = int(input())
+w_heap = []
+w_dict = dict()
+jid_dict = dict()
+hist_dict = dict()
+jdom_dict = dict()
+j_cnt = []
+
+def chk_domain(cur_dom, j_dom):
+    domain = cur_dom.split("/")[0]
+    if domain in j_dom:
+        return False
+    return True
+
+
+def chk_time(d_arg, t_arg, hist_d):
+    domain = d_arg.split("/")[0]
+    if domain in hist_d:
+        st = hist_d.get(domain)[0]
+        gap = hist_d.get(domain)[1] - st
+        if t_arg < st + 3 * gap:
+            return False
+    return True
+
+
+for _ in range(Q):
+    task = input().split()
+
+    if task[0] == "100":
+        j_cnt = [i for i in range(1, int(task[1])+1)]
+        heapq.heapify(j_cnt)
+        heapq.heappush(w_heap, (1, 0, task[2]))
+        w_dict[task[2]] = True
+
+    elif task[0] == "200":
+        if task[3] not in w_dict:
+            heapq.heappush(w_heap, (int(task[2]), int(task[1]), task[3]))
+            w_dict[task[3]] = True
+        else:
+            continue
+
+    elif task[0] == "300":
+        if len(j_cnt) == 0:
+            continue
+        # chk domain
+        # chk valid time(hist)
+        task_lst = []
+        while w_heap:
+            chk_task = heapq.heappop(w_heap)
+            if chk_domain(chk_task[2], jdom_dict) and chk_time(chk_task[2], int(task[1]), hist_dict):
+                w_dict.pop(chk_task[2])
+                cur_j_cnt = heapq.heappop(j_cnt)
+                jid_dict[cur_j_cnt] = (int(task[1]), chk_task[2].split("/")[0])
+                jdom_dict[chk_task[2].split("/")[0]] = True
+                break
+            else:
+                task_lst.append(chk_task)
+        w_heap = task_lst + w_heap
+        heapq.heapify(w_heap)
+
+    elif task[0] == "400":
+        end_t = int(task[1])
+        end_id = int(task[2])
+        heapq.heappush(j_cnt, end_id)
+        st, dom = jid_dict.get(end_id)
+        jid_dict.pop(end_id)
+        jdom_dict.pop(dom)
+        hist_dict[dom] = (st, end_t, end_id)
+
+    elif task[0] == "500":
+        print(len(w_heap))
