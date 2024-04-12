d101f525d9467bf38bc64f36c10567f621a7d674
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-04-12 11:27:13 +0900

    Title: 산타의 선물 공장 2, Time: 880ms, Memory: 44MB, Status: Wrong Answer - CodeTree

--- 240412/산타의 선물 공장 2/santa-gift-factory-2.py
+++ 240412/산타의 선물 공장 2/santa-gift-factory-2.py
@@ -1,6 +1,4 @@
-from collections import defaultdict
 import math
-import sys
 
 
 class Node:
@@ -10,138 +8,120 @@ class Node:
         self.next = None
 
 
-# sys.stdin = open("input.txt", "r")
 Q = int(input())
-st_ed_dict = defaultdict(list)
+
 box_dict = dict()
+belt_dict = dict()
 
 for _ in range(Q):
     task = list(map(int, input().split()))
-    fun = task[0]
 
-    if fun == 100:
-        n, m = task[1], task[2]
-        belt_dict = defaultdict(list)
-        cnt = 1
-        for i in range(3, m + 3):
-            box = Node(cnt)
-            if len(belt_dict[task[i]]):
-                box.prev = belt_dict[task[i]][-1]
-                belt_dict[task[i]][-1].next = box
-            belt_dict[task[i]].append(box)
-            box_dict[cnt] = box
-            cnt += 1
-        for bd in list(belt_dict.keys()):
-            st_ed_dict[bd] = [belt_dict[bd][0], belt_dict[bd][-1], len(belt_dict[bd])]
-
-    elif fun == 200:
-        src, dst = task[1], task[2]
-        if src not in st_ed_dict:
-            st_ed_dict[src] = [None, None, 0]
-        if dst not in st_ed_dict:
-            st_ed_dict[dst] = [None, None, 0]
-        if not (st_ed_dict[src][0] is None and st_ed_dict[src][1] is None):
-            src_lst = st_ed_dict[src]
-            if not (st_ed_dict[dst][0] is None and st_ed_dict[dst][1] is None):
-                dst_lst = st_ed_dict[dst]
-                dst_lst[0].prev = src_lst[1]
-                src_lst[1].next = dst_lst[0]
-                st_ed_dict[dst] = [st_ed_dict[src][0], st_ed_dict[dst][1], src_lst[2] + dst_lst[2]]
-                st_ed_dict[src] = [None, None, 0]
+    if task[0] == 100:
+        belt_t_num, box_t_num = task[1], task[2]
+        for bt_num in range(1, belt_t_num+1):
+            belt_dict[bt_num] = [None, None, 0]
+        t_num = 3
+        for bx_num in range(1, box_t_num+1):
+            new_node = Node(bx_num)
+            if belt_dict[task[t_num]][0] is None:
+                belt_dict[task[t_num]] = [new_node, new_node, 1]
             else:
-                st_ed_dict[dst] = [st_ed_dict[src][0], st_ed_dict[src][1], src_lst[2]]
-                st_ed_dict[src] = [None, None, 0]
-        print(st_ed_dict[dst][2])
+                f_node, l_node, length = belt_dict.get(task[t_num])
+                l_node.next = new_node
+                new_node.prev = l_node
+                belt_dict[task[t_num]] = [f_node, new_node, length + 1]
+            box_dict[bx_num] = new_node
+            t_num += 1
 
-    elif fun == 300:
+    elif task[0] == 200:
         src, dst = task[1], task[2]
-        src_lst = st_ed_dict[src] if src in st_ed_dict else [None, None, 0]
-        dst_lst = st_ed_dict[dst] if dst in st_ed_dict else [None, None, 0]
-        if src_lst[2] and dst_lst[2]:
-            s_next = src_lst[0].next
-            d_next = dst_lst[0].next
-            src_lst[0].next = d_next
-            dst_lst[0].next = s_next
+        if belt_dict[src][2]:
+            s_fst, s_lst, s_len = belt_dict.get(src)
+            if belt_dict[dst][2]:
+                d_fst, d_lst, d_len = belt_dict.get(dst)
+                s_lst.next = d_fst
+                d_fst.prev = s_lst
+                belt_dict[dst] = [s_fst, d_lst, s_len + d_len]
+            else:
+                belt_dict[dst] = [s_fst, s_lst, s_len]
+            belt_dict[src] = [None, None, 0]
+        print(belt_dict[dst][2])
+
+    elif task[0] == 300:
+        src, dst = task[1], task[2]
+        if belt_dict[src][2] and belt_dict[dst][2]:
+            s_change, s_lst, s_len = belt_dict.get(src)
+            s_next = s_change.next
+            d_change, d_lst, d_len = belt_dict.get(dst)
+            d_next = d_change.next
+            s_change.next = d_next
+            d_change.next = s_next
             if s_next:
-                s_next.prev = dst_lst[0]
+                s_next.prev = d_change
+                belt_dict[src] = [d_change, s_lst, s_len]
+            else:
+                belt_dict[src] = [d_change, d_change, 1]
             if d_next:
-                d_next.prev = src_lst[0]
-            d_tmp = dst_lst[0]
-            s_tmp = src_lst[0]
-            st_ed_dict[src][0] = d_tmp
-            if st_ed_dict[src][2] == 1:
-                st_ed_dict[src][1] = d_tmp
-            st_ed_dict[dst][0] = s_tmp
-            if st_ed_dict[dst][2] == 1:
-                st_ed_dict[dst][1] = s_tmp
-        elif src_lst[2] == 0 and dst_lst[2]:
-            d_next = dst_lst[0].next
-            dst_lst[0].next = None
+                d_next.prev = s_change
+                belt_dict[dst] = [s_change, d_lst, d_len]
+            else:
+                belt_dict[dst] = [s_change, s_change, 1]
+        elif belt_dict[src][2] == 0 and belt_dict[dst][2]:
+            d_change, d_lst, d_len = belt_dict[dst]
+            d_next = d_change.next
+            d_change.next = None
             if d_next:
                 d_next.prev = None
-                st_ed_dict[dst] = [d_next, dst_lst[1], dst_lst[2] - 1]
+                belt_dict[dst] = [d_next, d_lst, d_len - 1]
             else:
-                st_ed_dict[dst] = [None, None, 0]
-            st_ed_dict[src] = [dst_lst[0], dst_lst[0], 1]
-        elif dst_lst[2] == 0 and src_lst[2]:
-            s_next = src_lst[0].next
-            src_lst[0].next = None
+                belt_dict[dst] = [None, None, 0]
+            belt_dict[src] = [d_change, d_change, 1]
+        elif belt_dict[src][2] and belt_dict[dst][2] == 0:
+            s_change, s_lst, s_len = belt_dict[src]
+            s_next = s_change.next
+            s_change.next = None
             if s_next:
                 s_next.prev = None
-                st_ed_dict[src] = [s_next, src_lst[1], src_lst[2] - 1]
+                belt_dict[src] = [s_next, s_lst, s_len - 1]
             else:
-                st_ed_dict[src] = [None, None, 0]
-            st_ed_dict[dst] = [src_lst[0], src_lst[0], 1]
-        if dst in st_ed_dict:
-            print(st_ed_dict[dst][2])
-        else:
-            print(0)
+                belt_dict[src] = [None, None, 0]
+            belt_dict[dst] = [s_change, s_change, 1]
+        print(belt_dict[dst][2])
 
-    elif fun == 400:
+    elif task[0] == 400:
         src, dst = task[1], task[2]
-        if src not in st_ed_dict:
-            st_ed_dict[src] = [None, None, 0]
-        if dst not in st_ed_dict:
-            st_ed_dict[dst] = [None, None, 0]
-        move_len = math.floor(st_ed_dict[src][2]/2)
-        if not move_len:
-            pass
-        else:
-            start_box = st_ed_dict[src][0]
+        move_len = math.floor(belt_dict[src][2]/2)
+        if move_len:
+            s_fst, s_lst, s_len = belt_dict.get(src)
+            d_fst, d_lst, d_len = belt_dict.get(dst)
+            s_cur = s_fst
             for _ in range(1, move_len):
-                start_box = start_box.next
-            next_box = start_box.next
-            if st_ed_dict[dst][2]:
-                start_box.next = st_ed_dict[dst][0]
-                st_ed_dict[dst][0].prev = start_box
+                s_cur = s_cur.next
+            s_nxt = s_cur.next
+            if d_len:
+                d_fst.prev = s_cur
+                s_cur.next = d_fst
+                belt_dict[dst] = [s_fst, d_lst, d_len + move_len]
             else:
-                start_box.next = None
-            st_ed_dict[src][2] -= move_len
-            st_ed_dict[dst][2] += move_len
+                s_cur.next = None
+                belt_dict[dst] = [s_fst, s_cur, move_len]
+            if s_nxt is None or s_cur == s_lst:
+                belt_dict[src] = [None, None, 0]
+            else:
+                belt_dict[src] = [s_nxt, s_lst, s_len - move_len]
+        print(belt_dict[dst][2])
 
-            st_ed_dict[src][0] = next_box
-            next_box.prev = None
-            st_ed_dict[dst][0] = start_box
-            if not st_ed_dict[dst][1]:
-                st_ed_dict[dst][1] = start_box
-        print(st_ed_dict[dst][2])
-
-    elif fun == 500:
+    elif task[0] == 500:
         p_num = task[1]
         cur_box = box_dict[p_num]
-        prv = cur_box.prev.data if cur_box.prev else -1
-        nxt = cur_box.next.data if cur_box.next else -1
-        print(prv + 2 * nxt)
+        front = cur_box.prev.data if cur_box.prev else -1
+        end = cur_box.next.data if cur_box.next else -1
+        print(front + 2*end)
 
-    elif fun == 600:
+    elif task[0] == 600:
         b_num = task[1]
-        if b_num in st_ed_dict:
-            b_len = st_ed_dict[b_num][2]
-        else:
-            b_len = 0
-        if b_len:
-            front = st_ed_dict[b_num][0].data
-            end = st_ed_dict[b_num][1].data
-        else:
-            front, end = -1, -1
-        print(front + 2*end + 3*b_len)
+        c_fst, c_lst, c_len = belt_dict.get(b_num)
+        a = c_fst.data if c_fst else -1
+        b = c_lst.data if c_lst else -1
+        c = c_len
+        print(a + 2*b + 3*c)
