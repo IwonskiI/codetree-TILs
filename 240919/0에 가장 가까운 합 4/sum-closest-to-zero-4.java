3b5509aa0d706f929f93a01099882d86e77c8dac
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-09-19 22:08:35 +0900

    Title: 0에 가장 가까운 합 4, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240919/0에 가장 가까운 합 4/sum-closest-to-zero-4.java
+++ 240919/0에 가장 가까운 합 4/sum-closest-to-zero-4.java
@@ -3,55 +3,14 @@ import java.util.*;
 
 public class Main {
     
-	// CodeTree - 0에 가장 가까운 합
-	public static int N, min = Integer.MAX_VALUE;
-	public static int[] lst, ans = new int[3], num = new int[3];
-	public static boolean[] using;
-	
-	// 만들 수 있는 조합 계산
-	public static void combi(int start, int cnt) {
-		// 조합 완성됐다면
-		if(cnt == 3) {
-			// 현재 조합의 합 구하기
-			int sum = 0;
-			for(int i = 0; i < 3; i++) {
-				sum += num[i];
-			}
-			// 현재 총 합의 절댓값이 기존 최소값보다 작다면(0에 더 가깝다면)
-			if(Math.abs(sum) < min) {
-				// ans배열 갱신
-				for(int i = 0; i < 3; i++) {
-					ans[i] = num[i];
-				}
-				// 최솟값 갱신
-				min = Math.abs(sum);
-			}
-			return;
-		}
-		
-		// 시작점부터 사용중이지 않는 숫자들로 조합 구성
-		for(int i = start; i < N; i++) {
-			// 사용중이라면 스킵
-			if(using[i]) continue;
-			// 현재 위치에 i번째 숫자를 입력
-			num[cnt] = lst[i];
-			// 사용중 표시
-			using[i] = true;
-			// 다음 자리 조합 계산
-			combi(i+1, cnt+1);
-			// 탐색 완료 후 사용중 표시 해제
-			using[i] = false;
-		}
-	}
-	
+	// CodeTree - 0에 가장 가까운 합 4
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         
-        N = Integer.parseInt(br.readLine());
-        lst = new int[N];
-        using = new boolean[N];
+        int N = Integer.parseInt(br.readLine()), min = Integer.MAX_VALUE;
+        int[] lst = new int[N], ans = new int[3];
         st = new StringTokenizer(br.readLine());
         
         // 초기 입력
@@ -61,15 +20,49 @@ public class Main {
         // 오름차순 출력을 위한 정렬
         Arrays.sort(lst);
         
-        // 조합 계산
-        combi(0, 0);
-        
-        // 정답 저장
-        for(int i = 0; i < 3; i++) {
-        	sb.append(ans[i]).append(" ");
+		// 0~N-2까지 각 숫자를 첫 숫자로 기준점을 잡고 최솟갑 계산
+        for(int i = 0; i < N-2; i++) {
+			// 시작점은 i+1, 끝점은 가장 끝(N-1)
+        	int sp = i + 1, ep = N - 1;
+			// 시작점이 끝점보다 작을때까지 반복
+			// (시작점과 끝점이 교차하기 전까지)
+            while(sp < ep) {
+				// 현재 포인터의 세 정수 합과 절댓값 계산
+            	int sum = lst[i] + lst[sp] + lst[ep];
+				int abs = Math.abs(sum);
+            	
+				// 현재 합의 절댓값이 기존 최소 절댓갑보다 작다면
+            	if(abs < min) {
+					// 최솟값 갱신
+            		min = abs;
+					// 현재 자리에 저장된 수 저장
+            		ans[0] = lst[i];
+            		ans[1] = lst[sp];
+            		ans[2] = lst[ep];
+            	}
+				
+				// 합이 0보다 크다면 끝점을 앞으로 이동
+            	if(sum > 0) ep--;
+				// 합이 0보다 작다면 시작점을 뒤로 이동
+            	else if(sum < 0) sp++;
+				// 합이 0이라면
+            	else {
+					// 현재 위치에 저장된 숫자들을 출력후 종료
+            		sb.append(lst[i]).append(" ").append(lst[sp]).append(" ").append(lst[ep]);
+            		System.out.println(sb.toString());
+            		return;
+            	}
+            }
         }
         
+		// 탐색 종료 후 저장된 최소 합을 이루는 값 저장
+		for(int i = 0; i < 3; i++){
+			sb.append(ans[i]).append(" ");
+		}
+
         // 최종 결과 출력
         System.out.println(sb.toString());
+
+		return;
     }
 }
